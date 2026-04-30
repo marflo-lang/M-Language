a60d651a81622534ce61dfb74e76ce1318622e64
Marco F <gmarflo077@gmail.com>
2026-04-29 21:46:14 -0700

    - Implementación de más instrucciones IR y mejoras del compiler

--- compiler.c
+++ compiler.c
@@ -2,9 +2,122 @@
 #include "err.h"
 
 #include <malloc.h>
+#include <string.h>
 
+static char* getLexeme(Compiler* C, Token t)
+{
+    //return strndup();
+}
 
-static void ir_emit(IRList* list, IROpCode op, uint8_t a, uint8_t b, uint8_t c)
+static bool compare_tokens(Token a, Token b, const char* src)
+{
+    if (a.length != b.length) return false;
+
+    return strncmp(
+        src + a.location.begin.offset,
+        src + b.location.begin.offset,
+        a.length
+    ) == 0;
+}
+
+static Value make_int(int v)
+{
+    Value val;
+    val.type = VAL_INT;
+    val.i = v;
+    return val;
+}
+
+static Value make_float(double v)
+{
+    Value val;
+    val.type = VAL_FLOAT;
+    val.f = v;
+    return val;
+}
+
+static Value make_boolean(bool v)
+{
+    Value val;
+    val.type = VAL_BOOLEAN;
+    val.b = v;
+    return val;
+}
+
+static Value make_string(const char* src, int offset, int length)
+{
+    Value val;
+    val.type = VAL_STRING;
+    val.string.chars = src + offset;
+    val.string.length = length;
+    return val;
+}
+
+static Value make_nil()
+{
+    Value val;
+    val.type = VAL_NIL;
+    return val;
+}
+
+static Value make_nan()
+{
+    Value val;
+    val.type = VAL_NAN;
+    return val;
+}
+
+static Value token_to_value(Compiler* C, Token t)
+{
+    switch (t.type)
+    {
+        case M_V_INT:
+        {
+            char buffer[64];
+            int length = t.length;
+
+            if (length >= 64) length = 63;
+
+            memcpy(buffer, C->src+ t.location.begin.offset, length);
+            buffer[length] = '\0';
+
+            return make_int(atoi(buffer));
+        }
+        case M_V_FLOAT:
+        {
+            char buffer[64];
+            int length = t.length;
+
+            if (length >= 64) length = 63;
+
+            memcpy(buffer, C->src + t.location.begin.offset, length);
+            buffer[length] = '\0';
+
+            return make_float(atof(buffer));
+        }
+        case M_V_STRING:
+        {
+            return make_string(C->src, t.location.begin.offset + 1, t.length - 2);
+        }
+        case M_V_TRUE:
+        {
+            return make_boolean(true);
+        }
+        case M_V_FALSE:
+        {
+            return make_boolean(false);
+        }
+        case M_V_NIL:
+        {
+            make_nil();
+        }
+
+        default:
+            return make_nan();
+    }
+}
+
+static void ir_emit(IRList* list, IROpCode op, int a, int b, int c)
 {
     if (list->count >= list->capacity)
     {
@@ -22,16 +135,35 @@ static void ir_emit(IRList* list, IROpCode op, uint8_t a, uint8_t b, uint8_t c)
     list->data[list->count++] = instr;
 }
 
-static uint32_t alloc_reg(Compiler* C)
+static int const_add(ConstTable* T, Value v)
+{
+    if (T->count >= T->capacity)
+    {
+        T->capacity = T->capacity < 8 ? 8 : T->capacity * 2;
+        Value* newData = realloc(T->data, sizeof(Value) * T->capacity);
+        if (newData == NULL)
+        {
+            memoryCrash("Compile time");
+            exit(1);
+        }
+
+        T->data = newData;
+    }
+
+    T->data[T->count] = v;
+    return T->count++;
+}
+
+static int alloc_reg(Compiler* C)
 {
     return C->next_reg++;
 }
 
-static uint32_t symbol_define(SymbolTable* T, const char* name)
+static int symbol_define(SymbolTable* T, Token name, const char* src)
 {
-    for (uint32_t i = 0; i < T->count; i++)
+    for (int i = 0; i < T->count; i++)
     {
-        if (strcmp(T->names[i], name) == 0)
+        if (compare_tokens(T->names[i], name, src))
             return i;
     }
 
@@ -53,11 +185,11 @@ static uint32_t symbol_define(SymbolTable* T, const char* name)
     return T->count++;
 }
 
-static uint32_t symbols_resolve(SymbolTable* T, const char* name)
+static int symbols_resolve(SymbolTable* T, Token name, const char* src)
 {
-    for (uint32_t i = 0; i < T->count; i++)
+    for (int i = 0; i < T->count; i++)
     {
-        if (strcmp(T->names[i], name), name)
+        if (compare_tokens(T->names[i], name, src))
         {
             return i;
         }
@@ -66,11 +198,15 @@ static uint32_t symbols_resolve(SymbolTable* T, const char* name)
     return -1; // Error luego
 }
 
-void compiler_init(Compiler* C)
+void compiler_init(Compiler* C, const char* src, const char* name)
 {
     ir_init(&C->ir);
     symbols_init(&C->symbol);
+    constants_init(&C->constants);
+    C->src = src;
+    C->name = name;
     C->next_reg = 0;
+    C->next_const = 0;
 }
 
 void ir_init(IRList* list)
@@ -87,6 +223,13 @@ void symbols_init(SymbolTable* T)
     T->capacity = 0;
 }
 
+void constants_init(ConstTable* c)
+{
+    c->data = NULL;
+    c->capacity = 0;
+    c->count = 0;
+}
+
 int compiler_expr(Compiler* C, Expr* expr)
 {
     switch (expr->expr_type)
@@ -94,30 +237,68 @@ int compiler_expr(Compiler* C, Expr* expr)
         case EXPR_LITERAL:
         {
             LiteralExpr* literal = (LiteralExpr*) expr;
-            uint32_t r = alloc_reg(C);
+            int r = alloc_reg(C);
 
-            // Pendiente por revisar b
-            ir_emit(&C->ir, IR_LOAD_CONST, r, literal->value.location.begin.offset, 0);
-            break;
+            Value v = token_to_value(C, literal->value);
+
+            int k = const_add(&C->constants, v);
+
+            ir_emit(&C->ir, IR_LOAD_CONST, r, k, 0);
+            return r;
         }
         case EXPR_NAME:
         {
             NameExpr* nameE = (NameExpr*) expr;
             // Pendiente por revisar el name
-            uint32_t slot = symbols_resolve(&C->symbol, nameE->name.location.begin.offset);
+            int slot = symbols_resolve(&C->symbol, nameE->name, C->src);
 
-            uint32_t r = alloc_reg(C);
+            int r = alloc_reg(C);
             ir_emit(&C->ir, IR_LOAD_VAR, r, slot, 0);
-            break;
+            return r;
         }
         case EXPR_BINARY:
         {
+            BinaryExpr* binary = (BinaryExpr*) expr;
 
-            break;
+            int left = compiler_expr(C, binary->left);
+            int right = compiler_expr(C, binary->right);
+
+            int r = alloc_reg(C);
+
+            IROpCode op;
+
+            switch (binary->op.type)
+            {
+                case M_PLUS: op = IR_ADD; break;
+                case M_MINUS: op = IR_SUB; break;
+                case M_STAR: op = IR_MUL; break;
+                case M_SLASH: op = IR_DIV; break;
+                case M_FLOOR_DIV: op = IR_IDIV; break;
+                case M_MOD: op = IR_MOD; break;
+                case M_POW: op = IR_POW; break;
+                default: op = IR_ADD; break; // temporal
+            }
+
+            ir_emit(&C->ir, op, r, left, right);
+            return r;
         }
         case EXPR_UNARY:
         {
+            UnaryExpr* unary = (UnaryExpr*) expr;
 
+            int right = compiler_expr(C, unary->right);
+            int r = alloc_reg(C);
+
+            IROpCode op;
+
+            switch (unary->op.type)
+            {
+                case M_MINUS: op = IR_SUB; break; // temporal
+                //case M_NOT: op = 
+                default: op = IR_SUB; break; // temporal
+            }
+
+            ir_emit(&C->ir, op, r, 0, 0);
             break;
         }
         case EXPR_POSTFIX:
--- compiler.h
+++ compiler.h
@@ -1,73 +1,117 @@
 #pragma once
 #include "ast.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef enum
 {
+/*
+    name                args            description
+*/
     // Carga/Movimiento
-    IR_LOAD_CONST,
-    IR_LOAD_VAR,
-    IR_STORE_VAR,
-    IR_MOVE_VAR,
+    IR_LOAD_CONST,  //  A, B            R(A) = K(B)
+    IR_LOAD_VAR,    //  A, B            R(A) = S(B)
+    IR_STORE_VAR,   //  A, B            S(A) = R(B)
+    IR_MOVE,        //  A, B            R(B) = R(A) 
 
     // Operaciones Aritmeticas
-    IR_ADD,
-    IR_SUB,
-    IR_MUL,
-    IR_DIV,
-    IR_IDIV,
-    IR_MOD,
-    IR_POW,
+    IR_ADD,         // A, B, C        R(A) = R(B) + R(C)
+    IR_SUB,         // A, B, C        R(A) = R(B) - R(C)
+    IR_MUL,         // A, B, C        R(A) = R(B) * R(C)
+    IR_DIV,         // A, B, C        R(A) = R(B) / R(C)
+    IR_IDIV,        // A, B, C        R(A) = R(B) // R(C)
+    IR_MOD,         // A, B, C        R(A) = R(B) % R(C)
+    IR_POW,         // A, B, C        R(A) = R(B) ^ R(C)
 
     // Comparaciones
-    IR_EQ,
-    IR_NEQ,
-    IR_LT,
-    IR_LTE,
-    IR_GT,
-    IR_GTE,
+    IR_EQ,          // A, B, C        R(A) = R(B) == R(C)
+    IR_NEQ,         // A, B, C        R(A) = R(B) != R(C)
+    IR_LT,          // A, B, C        R(A) = R(B) < R(C)
+    IR_LTE,         // A, B, C        R(A) = R(B) <= R(C)
+    IR_GT,          // A, B, C        R(A) = R(B) > R(C)
+    IR_GTE,         // A, B, C        R(A) = R(B) >= R(C)
 
     // Control de flujo
-    IR_JUMP,
-    IR_JUMP_IF_FALSE,
+    IR_JUMP,        // Ax                 pc += Ax
+    IR_JUMP_IF_FALSE,   // A, Bx        if !A -> pc += Bx
 } IROpCode;
 
 typedef struct
 {
     IROpCode op;
 
-    uint8_t a;
-    uint8_t b;
-    uint8_t c;
+    int a;
+    int b;
+    int c;
 } IRInstruction;
 
 typedef struct
 {
     IRInstruction* data;
 
-    uint32_t count;
-    uint32_t capacity;
+    int count;
+    int capacity;
 } IRList;
+
+typedef enum
+{
+    VAL_NAN,
+    VAL_NIL,
+    VAL_INT,
+    VAL_FLOAT,
+    VAL_STRING,
+    VAL_BOOLEAN
+} ValueType;
 
 typedef struct
 {
-    const char** names;
-    uint32_t count;
-    uint32_t capacity;
+    ValueType type;
+
+    union
+    {
+        int i;
+        double f;
+        bool b;
+
+        struct
+        {
+            const char* chars;
+            int length;
+        } string;
+    };
+} Value;
+
+typedef struct
+{
+    Token* names;
+    int count;
+    int capacity;
 } SymbolTable;
 
 typedef struct
 {
+    Value* data;
+    int count;
+    int capacity;
+} ConstTable;
+
+typedef struct
+{
     IRList ir;
+    const char* src;
+    const char* name;
 
     SymbolTable symbol;
-    uint32_t next_reg;
+    ConstTable constants;
+    int next_reg;
+    int next_const;
 } Compiler;
 
 
 void ir_init(IRList* list);
 void symbols_init(SymbolTable* T);
-void compiler_init(Compiler* C);
+void constants_init(ConstTable* c);
+void compiler_init(Compiler* C, const char* src, const char* name);
 int compiler_expr(Compiler* C, Expr* expr);
 void compiler_stmt(Compiler* C, Stmt* stmt);
 
